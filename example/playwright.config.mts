3c127126d4b5cb79470c0f19c8bd34f97084fcb9
nerdyman <averynerdyman@gmail.com>
2024-09-21 15:33:06 +0100

    test: move tests into example folder and add granular interactivity tests

--- example/playwright.config.mts
+++ example/playwright.config.mts
@@ -1,6 +1,5 @@
 /* eslint no-console: 0 */
-import { devices } from '@playwright/test';
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const IS_CI = !!process.env.CI;
 const PORT = !isNaN(Number(process.env.PORT)) ? Number(process.env.PORT) : 3000;
@@ -12,16 +11,15 @@ console.info('[playwright.config]', { IS_CI, PORT });
 const config: PlaywrightTestConfig = {
   forbidOnly: IS_CI,
   retries: IS_CI ? 2 : 0,
-  testDir: './test',
-  outputDir: './test/results',
+  testDir: './',
+  outputDir: './tests/results',
   reporter: 'list',
   webServer: {
-    command: 'pnpm run build && pnpm --filter "react-image-turntable-example" run start',
+    command: 'pnpm run start',
     reuseExistingServer: !IS_CI,
     url: `http://localhost:${PORT}`,
     env: {
       NODE_ENV: 'test',
-      USE_BABEL_PLUGIN_ISTANBUL: '1',
     },
   },
   use: {
@@ -35,15 +33,15 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-  ],
+    IS_CI && {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    IS_CI && {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ].filter(Boolean) as PlaywrightTestConfig['projects'],
 };
 
 export default config;
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,66 +1,75 @@
-import { ReactImageTurntable, useReactImageTurntable } from 'react-image-turntable';
-import { AdvancedDemo } from './AdvancedDemo';
-import { useState } from 'react';
+import { AdvancedDemo } from './demos/Advanced';
+import { BasicDemo } from './demos/Basic';
 
-export const images = [
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_01.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_02.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_03.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_04.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_05.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_06.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_07.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_08.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_09.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_10.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_11.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_12.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_13.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_14.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_15.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_16.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_17.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_18.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_19.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_20.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_21.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_22.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_23.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_24.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_25.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_26.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_27.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_28.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_29.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_30.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_31.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_32.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_33.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_34.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_35.jpg?v=1',
-  'https://static2.zerolight.com/web/df3a45687480167bb4451d79c02f67bd/img/2d-explorer/bmw/P0C1G__S022B__FX3A9/P0C1G__S022B__FX3A9__Spin_36.jpg?v=1',
+import {
+  createBrowserRouter,
+  Link,
+  type LinkProps,
+  RouterProvider,
+  useLocation,
+} from 'react-router-dom';
+import ErrorPage from './ErrorPage';
+import { type FC } from 'react';
+
+const links: LinkProps[] = [
+  { children: 'Advanced', to: '/' },
+  { children: 'Basic', to: '/basic' },
 ];
 
-/**
- * Basic demo with required props only.
- */
-const Demo = () => {
-  const turntableProps = useReactImageTurntable({ images });
+const Nav: FC = () => {
+  const location = useLocation();
 
-  return <ReactImageTurntable {...turntableProps} />;
-};
-
-export const App = () => {
-  const [showAdvancedDemo, setShowAdvancedDemo] = useState(true);
+  if (new URLSearchParams(location.search).get('noNav') === 'true') {
+    return false;
+  }
 
   return (
-    <div>
-      <div className="main__toggle">
-        <button onClick={() => setShowAdvancedDemo(!showAdvancedDemo)}>
-          {showAdvancedDemo ? 'Show basic mode' : 'Show dev mode'}
-        </button>
-      </div>
-      {showAdvancedDemo ? <AdvancedDemo /> : <Demo />}
+    <nav className="nav">
+      <ul className="nav-list">
+        {links.map((link) => (
+          <li className="nav-list__item" key={link.to as string}>
+            <Link
+              {...link}
+              className="nav-list__link"
+              aria-current={location.pathname === link.to ? true : undefined}
+            />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Nav />
+        <AdvancedDemo />
+      </>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/basic',
+    element: (
+      <>
+        <Nav />
+        <div className="main__turntable">
+          <BasicDemo />
+        </div>
+      </>
+    ),
+    errorElement: <ErrorPage />,
+  },
+]);
+
+export const App = () => {
+  return (
+    <div className="main">
+      <h1 className="sr-only">React Image Turntable</h1>
+      <RouterProvider router={router} />
     </div>
   );
 };
